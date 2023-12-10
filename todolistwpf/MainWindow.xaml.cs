0e681812db9da91374719d482f0bd9bb453edcde
Kamil <125757644+httpsDr3aMy@users.noreply.github.com>
2023-12-10 09:36:00 +0100

    Dodaj pliki projektów.

---
+++ todolistwpf/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace todolistwpf
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ToDoItem> ToDoItems { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ToDoItems = new ObservableCollection<ToDoItem>();
+            ToDoListView.ItemsSource = ToDoItems;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string title = TitleTextBox.Text;
+            string description = DescriptionTextBox.Text;
+
+            if (!string.IsNullOrEmpty(title))
+            {
+                ToDoItems.Add(new ToDoItem { Title = title, Description = description });
+                TitleTextBox.Clear();
+                DescriptionTextBox.Clear();
+            }
+        }
+
+        private void NewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TitleTextBox.Clear();
+            DescriptionTextBox.Clear();
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ToDoListView.SelectedItem != null)
+            {
+                ToDoItem selectedToDo = (ToDoItem)ToDoListView.SelectedItem;
+                TitleTextBox.Text = selectedToDo.Title;
+                DescriptionTextBox.Text = selectedToDo.Description;
+                ToDoItems.Remove(selectedToDo);
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ToDoListView.SelectedItem != null)
+            {
+                ToDoItem selectedToDo = (ToDoItem)ToDoListView.SelectedItem;
+                ToDoItems.Remove(selectedToDo);
+            }
+        }
+    }
+
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
